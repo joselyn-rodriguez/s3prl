fixing bug - wrong dimensionality

s3prl/upstream/mockingjay/expert.py
@@ -73,10 +73,6 @@ class UpstreamExpert(UpstreamBase):
     #     print("len after unbinding hidden states: ", len(hidden_states.unbind(dim=0)))
     #     print("Type of after unbinding hidden_states: ", type(hidden_states.unbind(dim=0)))
     #     print("first item after unbinding of hidden states: ", hidden_states.unbind(dim=0)[0])
-
-
-
-
     #     return {
     #         "last_hidden_state": last_hidden_state,
     #         "hidden_states": hidden_states.unbind(dim=0),
@@ -89,26 +85,33 @@ class UpstreamExpert(UpstreamBase):
             wavs
         )  # (batch_size, extracted_seqlen, feature_dim)
 
+        # print('inside expert.py forward: \n')
+        # print('shape of encoded_layers: ', len(encoded_layers) )
+        # print(f' shape of encoded_layers[0]: {len(encoded_layers[0])} ')
+        # print(f' shape of encoded_layers[0]: {len(encoded_layers[1])} ')
+        # print(f' shape of encoded_layers[0]: {len(encoded_layers[2])} ')
+        # print(f' shape of encoded_layers[0]: {len(encoded_layers[3])} ')
+
         attentions = encoded_layers[0]
         keys = encoded_layers[1]
         queries = encoded_layers[2]
-        hidden_states = torch.stack(encoded_layers[1])
+        hidden_states = torch.stack(encoded_layers[3])
 
-        print("---------")
-        print("Len of attentions: ", len(attentions))
-        print("Type of attentions: ", type(attentions))
+        # print("---------")
+        # print("Len of attentions: ", len(attentions))
+        # print("Type of attentions: ", type(attentions))
 
-        print("---------")
-        print("Len encoded_layers: ", len(encoded_layers))
-        print("Type encoded_layers: ", type(encoded_layers))
-        print("Len encoded_layers[0] (same as attentions): ", len(encoded_layers[0]))
-        print("Type of encoded_layers[0] (same as attentions): ", type(encoded_layers[0]))
-        print("Len of encoded_layers[1] : ", type(encoded_layers[1]))
-        print("Len of encoded_layers[2] : ", type(encoded_layers[2]))
+        # print("---------")
+        # print("Len encoded_layers: ", len(encoded_layers))
+        # print("Type encoded_layers: ", type(encoded_layers))
+        # print("Len encoded_layers[0] (same as attentions): ", len(encoded_layers[0]))
+        # print("Type of encoded_layers[0] (same as attentions): ", type(encoded_layers[0]))
+        # print("Len of encoded_layers[1] : ", type(encoded_layers[1]))
+        # print("Len of encoded_layers[2] : ", type(encoded_layers[2]))
 
-        # this type should be a tensor! where is the type being changed?
-        print("Len encoded_layers[1] (hidden_states): ", len(encoded_layers[1]))
-        print("Type of encoded_layers[1] (hidden_states): ", type(encoded_layers[1]))
+        # # this type should be a tensor! where is the type being changed?
+        # print("Len encoded_layers[1] (hidden_states): ", len(encoded_layers[1]))
+        # print("Type of encoded_layers[1] (hidden_states): ", type(encoded_layers[1]))
 
         # print("inside EXPERT: last item in encoded layers [3]: ")
         # print(hidden_states[3][0])

s3prl/upstream/mockingjay/model.py
@@ -144,7 +144,7 @@ class TransformerSelfAttention(nn.Module):
 
         # initializing key and queries
         self.query = nn.Linear(config.hidden_size, self.all_head_size)
-        print('query: ', self.query)
+        # print('query: ', self.query)
         self.key = nn.Linear(config.hidden_size, self.all_head_size)
         self.value = nn.Linear(config.hidden_size, self.all_head_size)
 
@@ -159,7 +159,7 @@ class TransformerSelfAttention(nn.Module):
         return x.permute(0, 2, 1, 3)
 
     def forward(self, hidden_states, attention_mask, head_mask=None):
-        print('hidden states: ', hidden_states.shape)
+        # print('hidden states: ', hidden_states.shape)
         mixed_query_layer = self.query(hidden_states)
         mixed_key_layer = self.key(hidden_states)
         mixed_value_layer = self.value(hidden_states)
@@ -269,7 +269,7 @@ class TransformerAttention(nn.Module):
         if self.output_attentions:
             attentions, self_output = self_output
         if self.output_key_query:
-            print("self output: ", len(self_output))
+            # print("self output: ", len(self_output))
             attentions, keys, queries, self_output = self_output
 
         attention_output = self.output(self_output, input_tensor)
@@ -410,7 +410,6 @@ class TransformerEncoder(nn.Module):
                 all_attentions.append(attentions)
                 all_keys.append(keys)
                 all_queries.append(queries)
-                print('here within TransformerEncoder')
 
         if self.pre_layer_norm:
             all_encoder_layers.append(self.LayerNorm[-1](hidden_states))
@@ -652,7 +651,7 @@ class TransformerModel(TransformerInitModel):
             # print("type of encoded layers[1]: ", type(encoded_layers[1][0]))
 
             # print("in MODEL: last item in encoded layers [3]: ")
-            print(encoded_layers[3][0])
+            # print("shape of hidden layers: ", encoded_layers[3][0].shape)
 
         if not output_all_encoded_layers:
             encoded_layers = encoded_layers[-1]